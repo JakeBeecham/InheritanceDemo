80da608e3d9cc52f28dad5ab0737a1d5c555df9f
Jake Beecham <60218222+JakeBeecham@users.noreply.github.com>
2020-02-26 10:35:28 +0000

    Exercise Started
    Weapon Parent Class and Pistol Child Class created, along with bullet script.

---
+++ InheritanceDemo/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public Weapon activeWeapon;
+
+	void Start ()
+    {
+        activeWeapon = Instantiate(activeWeapon, transform);
+        activeWeapon.transform.position = transform.position;
+	}
+	
+	void Update ()
+    {
+        //if activeWeapon is not null (has the weapon prefab been cloned)
+        if (activeWeapon)
+        {
+            if (activeWeapon.IsAutomatic)
+            {
+                if (Input.GetButtonDown("Fire1"))//left mouse button and right controller trigger
+                {
+                    if (activeWeapon.HasAmmo())
+                        activeWeapon.Fire(transform.position);
+                }
+            }
+            else
+            {
+                if (Input.GetButtonDown("Fire1"))//left mouse button and right controller trigger
+                {
+                    if (activeWeapon.HasAmmo())
+                        activeWeapon.Fire(transform.position);
+                }
+            }
+        }
+	}
+}
